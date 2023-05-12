09b66c37f4528db5b45882464f6a12e604af96f0
dagbay <80764810+dagbay@users.noreply.github.com>
2023-05-12 12:09:55 +1000

    Implement Google OAuth

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <main>
+        <div className="bg-blue-900 w-screen h-screen flex items-center">
+          <div className="text-center w-full">
+            <button
+              className="bg-white p-2 px-4 rounded-sm"
+              onClick={() => signIn("google")}
+            >
+              Login with Google
+            </button>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main>
       <div className="bg-blue-900 w-screen h-screen flex items-center">
         <div className="text-center w-full">
-          <button className="bg-white p-2 px-4 rounded-sm">
-            Login with Google
+          <p>Signed in as {session.user.email}</p>
+          <button
+            className="bg-white p-2 px-4 rounded-sm"
+            onClick={() => signOut()}
+          >
+            Logout
           </button>
         </div>
       </div>
